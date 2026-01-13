b9a34426c882c31f96087b2145c8bd2a5abf1f22
Yiannis Hadjimichael <hadjimichael@wias-berlin.de>
2026-01-13 17:52:04 +0100

    Add ElectroMechanicsFEM.jl

---
+++ src/ElectroMechanicsFEM.jl
@@ -0,0 +1,119 @@
+module ElectroMechanicsFEM
+
+using DocStringExtensions: DocStringExtensions, TYPEDEF
+using ExtendableFEM: ExtendableFEM, ItemIntegrator, NonlinearOperator,
+                    assemble!, fixed_dofs, grad, id
+using ExtendableFEMBase: ExtendableFEMBase, CellDofs, FEEvaluator, FEMatrix,
+                        FESpace, FEVector, FEVectorBlock, Gradient, H1P1,
+                        Identity, L2P0, PointEvaluator, QPInfos,
+                        QuadratureRule, continuify, displace_mesh,
+                        displace_mesh!, evaluate!, evaluate_bary!, fill!,
+                        get_ncomponents, get_ndofs, get_polynomialorder,
+                        lazy_interpolate!, nodevalues, nodevalues!,
+                        nodevalues_view, update_basis!
+using ExtendableGrids: ExtendableGrids, BFaceGeometries, BFaceNodes,
+                        BFaceRegions, Cartesian2D, CellFinder, CellGeometries,
+                        CellNodes, CellRegions, CellVolumes, CoordinateSystem,
+                        Coordinates, Edge1D, ElementGeometries, ExtendableGrid,
+                        FaceCells, FaceEdges, FaceGeometries, FaceNodes,
+                        ON_CELLS, PColorPartitions, PartitionCells,
+                        Tetrahedron3D, Triangle2D, UniqueBFaceGeometries,
+                        UniqueCellGeometries, UniqueFaceGeometries,
+                        VectorOfConstants, append!, bfacemask!, cellmask!,
+                        gFindBruteForce!, gFindLocal!, glue, interpolate!,
+                        local_celledgenodes, max_num_targets_per_source,
+                        num_cells, num_partitions, num_pcolors, num_sources,
+                        subgrid, uniform_refine, unique
+using ExtendableSparse: ExtendableSparse, ExtendableSparseMatrix, factorize!,
+                        flush!, rawupdateindex!
+using ForwardDiff: ForwardDiff, DiffResults
+using GridVisualize: GridVisualize, GridVisualizer, gridplot, scalarplot,
+                    scalarplot!
+using LinearAlgebra: LinearAlgebra, /, cross, det, dot, ldiv!, mul!, norm,
+                    transpose
+using LinearSolve: LinearSolve, LinearProblem, init
+using Printf: Printf, @printf
+using SimplexGridFactory: SimplexGridFactory, SimplexGridBuilder, cellregion!,
+                        facet!, facetregion!, maxvolume!, options!, point!,
+                        regionpoint!, simplexgrid
+using SparseArrays: SparseArrays, SparseMatrixCSC, sparse
+using SparseDiffTools: SparseDiffTools, ForwardColorJacCache,
+                        forwarddiff_color_jacobian!, matrix_colors
+using TetGen: TetGen
+using Triangulate: Triangulate
+
+## problem: loading and saving grids leads to ElementGeometries -> DataType conversion (by DrWatson/JLD2?) which has to be reverted
+## after loading (until this is fixed ina more elegant way)
+function repair_grid!(xgrid::ExtendableGrid)
+    xgrid.components[CellGeometries] = VectorOfConstants{ElementGeometries,Int}(xgrid.components[CellGeometries][1], num_cells(xgrid))
+    xgrid.components[FaceGeometries] = VectorOfConstants{ElementGeometries,Int}(xgrid.components[FaceGeometries][1], length(xgrid.components[FaceGeometries]))
+    xgrid.components[BFaceGeometries] = VectorOfConstants{ElementGeometries,Int}(xgrid.components[BFaceGeometries][1], length(xgrid.components[BFaceGeometries]))
+
+    xgrid.components[UniqueCellGeometries] = Vector{ElementGeometries}([xgrid.components[CellGeometries][1]])
+    xgrid.components[UniqueFaceGeometries] = Vector{ElementGeometries}([xgrid.components[FaceGeometries][1]])
+    xgrid.components[UniqueBFaceGeometries] = Vector{ElementGeometries}([xgrid.components[BFaceGeometries][1]])
+end
+export repair_grid!
+
+include("grids.jl")
+export bimetal_strip3D, bimetal_strip2D, bimetal_strip3D_middle_layer, bimetal_tensorgrid, bimetal_tensorgrid_uniform, bimetal_tensorgrid_uniform!
+export condensator3D, condensator3D_tensorgrid, condensator3D_tensorgrid!, condensator2D
+export nonpolarquantumwell3D, nonpolarquantumwell2D
+export nanowire_grid, nanowire_tensorgrid, nanowire_tensorgrid_mirror
+
+include("materialstructuretype.jl")
+export MaterialStructureType
+export ZincBlende2D, ZincBlende001, ZincBlende111_C14, ZincBlende111_C15, ZincBlende111_C14_C15, Wurtzite0001, Wurtzite
+
+include("materials.jl")
+export MaterialType
+export TestMaterial, GaAs, AlInAs, AlGaAs, InGaAs, GaN, InGaN
+export MaterialParameters
+export get_materialtype
+
+include("tensors.jl")
+export AbstractTensor
+export DenseMatrixTensor, SparseMatrixTensor
+export IsotropicElasticityTensor
+export apply_tensor!
+
+
+include("heterostructures.jl")
+export HeteroStructureData
+
+include("strains.jl")
+export StrainType
+export LinearStrain, LinearStrain2D, LinearStrain3D
+export NonlinearStrain, NonlinearStrain2D, NonlinearStrain3D
+export PreStrainType
+export IsotropicPrestrain, AnisotropicDiagonalPrestrain
+export eval_strain!, eval_elastic_strain!
+
+include("pdeoperators.jl")
+export PDEDisplacementOperator, PDEPolarisationOperator
+export get_displacement_operator, get_displacement_operator
+export get_polarisation_from_strain_operator
+export get_polarisation_laplacian_operator
+export get_polarisation_operator # wip
+export get_energy_integrator
+
+include("plane_cuts.jl")
+export tet_x_plane!, plane_cut, get_cutgrids
+export perform_simple_plane_cuts, perform_plane_cuts
+
+include("export.jl")
+export exportVTK
+
+include("statistics.jl")
+export compute_statistics
+
+include("solvers.jl")
+export solve_lowlevel, solve_lowlevel_parallel
+
+include("solvers_from_energy.jl")
+export EnergyOperator, update_M!
+export ∂FW!
+
+greet() = print("Hello World!")
+
+end # module
